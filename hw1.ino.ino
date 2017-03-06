d0576d3566db0b813e752a5578c059b13e90a627
kataYEH <xxszcvfh@gmail.com>
2017-03-06 19:23:39 +0800

    HW3
    開關控制 HW1 HW2

--- hw1.ino.ino
+++ hw1.ino.ino
@@ -1,5 +1,6 @@
 const byte LEDs[]={0,1,2,3,4,5,6,7};
 const byte total = sizeof(LEDs);
+const byte SW = 13;
 byte index = 0;
 byte count = 0;
 byte light=0;
@@ -10,73 +11,115 @@ void setup()
   {
     pinMode(LEDs[i],OUTPUT);
   }
-
+  pinMode(SW,INPUT);
 }
 void loop() 
 {
+  boolean val = digitalRead(13);
   // put your main code here, to run repeatedly:
   //所有OFF
-  for(byte i=0;i<total;i++)
-    {
-    digitalWrite(LEDs[i],LOW);
-    }
-  //亮index
-    digitalWrite(LEDs[index],HIGH);
-    
-  //
-  if(count == 0)
+  if(!val)
   {
-      if(index<total)
-    {
-      index++;
-    }
-      else if(index==total && light <2)
-     {
-       for(byte i2=0;i2<total;i2++)
+          // put your main code here, to run repeatedly:
+        //所有OFF
+        for(byte i=0;i<total;i++)
+          {
+          digitalWrite(LEDs[i],LOW);
+          }
+        //亮index
+          digitalWrite(LEDs[index],HIGH);
+          
+        //
+        if(count == 0)
         {
-        digitalWrite(LEDs[i2],HIGH);
+            if(index<total)
+          {
+            index++;
+          }
+          else
+          {
+            count = 1;
+          }
+            delay(500);
         }
-        delay(500);
-      light++;
-        for(byte i2=0;i2<total;i2++)
+        else
         {
-        digitalWrite(LEDs[i2],LOW);
-        }
-        
-     }
-      else
-    {
-      count = 1;
-      light = 0;
-    }
-      delay(500);
-  }
+          if(index >0)
+          {
+            index--;
+            }
+           else
+           {
+            count = 0;
+            }
+            delay(500);
+         }
+           
+    }// 最大
+
   else
   {
-    if(index >0)
-    {
-      index--;
-      }
-     else if(index == 0 && light < 2 )
-     {
-        for(byte i2=0;i2<total;i2++)
+        for(byte i=0;i<total;i++)
+          {
+          digitalWrite(LEDs[i],LOW);
+          }
+        //亮index
+          digitalWrite(LEDs[index],HIGH);
+          
+        //
+        if(count == 0)
         {
-          digitalWrite(LEDs[i2],HIGH);
+            if(index<total)
+          {
+            index++;
+          }
+            else if(index==total && light <2)
+           {
+             for(byte i2=0;i2<total;i2++)
+              {
+              digitalWrite(LEDs[i2],HIGH);
+              }
+              delay(500);
+            light++;
+              for(byte i2=0;i2<total;i2++)
+              {
+              digitalWrite(LEDs[i2],LOW);
+              }
+              
+           }
+            else
+          {
+            count = 1;
+            light = 0;
+          }
+            delay(500);
         }
-       delay(500);
-        light++;
-       for(byte i2=0;i2<total;i2++)
+        else
         {
-        digitalWrite(LEDs[i2],LOW);
-        }
-       
-      }
-     else
-     {
-      light = 0; 
-      count = 0;
-      }
-      delay(500);
-   }
-     
+          if(index >0)
+          {
+            index--;
+            }
+           else if(index == 0 && light < 2 )
+           {
+              for(byte i2=0;i2<total;i2++)
+              {
+                digitalWrite(LEDs[i2],HIGH);
+              }
+             delay(500);
+              light++;
+             for(byte i2=0;i2<total;i2++)
+              {
+              digitalWrite(LEDs[i2],LOW);
+              }
+             
+            }
+           else
+           {
+            light = 0; 
+            count = 0;
+            }
+            delay(500);
+         }
+    }//最大ELSE    
 }
